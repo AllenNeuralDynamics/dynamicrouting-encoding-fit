9cbb2f558962b9148d325f954f6cecc23b65c089
shailajaAkella <68871896+shailajaAkella@users.noreply.github.com>
2025-02-17 16:26:36 -0800

    Bug fix

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -246,7 +246,7 @@ def main():
         data_path = utils.get_data_root()
 
     # test logging
-    logger.info(f"Files in {data_path}: {os.listdir(data_path)})
+    logger.info(f"Files in {data_path}: {os.listdir(data_path)}")
 
     input_dict_paths = tuple(data_path.rglob('*_inputs.npz'))
     logger.info(f"Found {len(input_dict_paths)} inputs .npz file(s) in {data_path}")
